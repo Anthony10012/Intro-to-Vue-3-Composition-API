f17103b6f88018707cf495d45d1bd3aceba211c4
Anthony Simond <pv22nbo@eduvaud.ch>
2025-11-03 14:08:08 +0100

    Exo 2 done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
-<script setup></script>
+<script setup>
+import socksImage from "./assets/images/socks_green.jpeg"
+
+const product = "Socks"
+const image = socksImage
+const description = "Voici une chaussette "
+const url = "https://www.cpnv.ch" 
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{product}}</h1>
+        <p>{{description}}</p>
+        <a :href="url" >Visiter le site du CPNV</a>
+        <div class="product-image">
+          <img :src="image" alt="Image des chaussettes"/>
+        </div> 
       </div>
     </div>
   </div>
 </template>
+

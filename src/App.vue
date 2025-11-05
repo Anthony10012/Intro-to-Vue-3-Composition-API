2e08ef7cc445fdb25a539ea8755687b37b84ff00
Anthony Simond <pv22nbo@eduvaud.ch>
2025-11-05 11:47:20 +0100

    Exo 4 done

--- src/App.vue
+++ src/App.vue
@@ -21,18 +21,25 @@ const sizes = ref(["S","M","L","XL",])
         <h1>{{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
-        <h2>Composants</h2>
-      <ul>
-        <li v-for="detail in details" :key="detail">{{detail}}</li>
-      </ul>
-      <h3>Couleurs disponibles</h3>
-      <div class="colors">
-        <button class="button" v-for="variant in variants" :key="variant.id" :style="{backgroundColor: variant.color}"></button>
+        <div class="product-options">
+        <div class="left-column">
+          <h2>Composants</h2>
+          <ul>
+            <li v-for="detail in details" :key="detail">{{detail}}</li>
+          </ul>
+          <h3>Couleurs disponibles</h3>
+          <div class="colors">
+            <button class="button" v-for="variant in variants" :key="variant.id" :style="{backgroundColor: variant.color}"></button>
+          </div>
+        </div>
+
+        <div class="right-column">
+          <h3>Tailles</h3>
+          <ul>
+            <li v-for="size in sizes" :key="size">{{size}}</li>
+          </ul>
+        </div>
       </div>
-      <h3 class="size">Tailles</h3>
-      <ul>
-        <li v-for="size in sizes" :key="size">{{size}}</li>
-      </ul>
       </div>
     </div>
   </div>

3efa6f90265dde4b6c32ccdbfb99364116764607
Anthony Simond <pv22nbo@eduvaud.ch>
2025-11-10 14:05:16 +0100

    Exo 8 done

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="product-details">
+    <h3>Product Details:</h3>
+    <ul>
+      <li v-for="(detail, index) in details" :key="index">{{ detail }}</li>
+    </ul>
+  </div>
+</template>
+
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  details: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.product-details ul {
+  padding-left: 20px;
+}
+</style>

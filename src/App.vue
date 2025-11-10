45cb324aa110b0b5505f232e648743149db5e924
Anthony Simond <pv22nbo@eduvaud.ch>
2025-11-10 11:09:44 +0100

    Exo 7 done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <script setup>
-import { ref } from 'vue'
+import { ref , computed } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
 import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
-const brand = ref('Vue Mastery')
+const brand = ref('CPNV')
+const onSale = ref(true)
 
-const image = ref(socksGreenImage)
-const inStock = ref(false)
+const title = computed(()=>{
+  return brand.value + ' ' + product.value
+})
+
+
+
   
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage },
-  { id: 2235, color: 'blue', image: socksBlueImage },
+  { id: 2234, color: 'green', image: socksGreenImage,quantity:10 },
+  { id: 2235, color: 'blue', image: socksBlueImage, quantity:0},
 ])
 
 const cart = ref(0)
 
 const addToCart = () => cart.value += 1
 
-const updateImage = (variantImage) => image.value = variantImage
+const selectedVariant = ref(0)
+const updateVariant = (index) => selectedVariant.value = index
+
+const image = computed(()=>{
+  return variants.value[selectedVariant.value].image
+})
+
+const inStock = computed(()=>{
+  return variants.value[selectedVariant.value].quantity > 0
+})
+
+const sale = computed(()=>{
+  return onSale.value ? ' est en action' : ''
+})
 
 </script>
   
@@ -33,16 +51,17 @@ const updateImage = (variantImage) => image.value = variantImage
         <img v-bind:src="image">
       </div>
       <div class="product-info">
-        <h1>{{ product }}</h1>
+        <h1>{{ title }}</h1>
+        <p> {{ sale }}</p>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
         <div 
-          v-for="variant in variants" 
+          v-for="(variant, index) in variants" 
           :key="variant.id"
-          @mouseover="updateImage(variant.image)"
+          @mouseover="updateVariant(index)"
           class="color-circle"
           :style="{ backgroundColor: variant.color }"
         >
